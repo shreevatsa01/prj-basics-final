5be5f9be40133e9ea337cd12e727a04a06e4c132
Shreevatsa <shreevatsa.1si12cs152@gmail.com>
2018-11-24 19:34:15 +0530

    Commiting recipe component changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -10,12 +10,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Anathor Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe:Recipe) {
+  this.recipeWasSelected.emit(recipe);
   }
 
 }
